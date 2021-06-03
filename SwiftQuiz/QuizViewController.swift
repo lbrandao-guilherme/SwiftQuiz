c866e41044c5cb2fea299d0e27cda5643af545b8
lgsbrandao <40247614+lbrandao-guilherme@users.noreply.github.com>
2021-06-03 16:14:34 -0300

    Fix timer bug

--- SwiftQuiz/QuizViewController.swift
+++ SwiftQuiz/QuizViewController.swift
@@ -20,9 +20,45 @@ class QuizViewController: UIViewController {
 
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        timerView.frame.size.width = view.frame.size.width
+        UIView.animate(withDuration: 60.0, delay: 0, options: .curveLinear, animations: {
+            self.timerView.frame.size.width = 0
+        }) { (success) in
+            self.showResults()
+        }
+        
+        getNewQuiz()
+    }
+    
+    func getNewQuiz() {
+        quizManager.refreshQuiz()
+        QuestionLB.text = quizManager.question
+        
+        for i in 0..<quizManager.options.count {
+            let option = quizManager.options[i]
+            let button = AnswerBT[i]
+            button.setTitle(option, for: .normal)
+        }
+    }
+    
+    func showResults() {
+        performSegue(withIdentifier: "resultSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController = segue.destination as! ResultViewController
+        resultViewController.totalAnswers = quizManager.totalAnswer
+        resultViewController.totalCorrectAnswers = quizManager.totalCorrectAnswer
+    }
+    
 
     @IBAction func selectAnswer(_ sender: UIButton) {
-        
+        let index = AnswerBT.firstIndex(of: sender)!
+        quizManager.validadeAnswer(index: index)
+        getNewQuiz()
     }
     
 }
--- SwiftQuiz/ResultViewController.swift
+++ SwiftQuiz/ResultViewController.swift
@@ -14,14 +14,23 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var WrongLB: UILabel!
     @IBOutlet weak var ScoreLB: UILabel!
     
+    var totalCorrectAnswers: Int = 0
+    var totalAnswers: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        AnswerdLB.text = "Perguntas respondidas: \(totalAnswers)"
+        CorrectLB.text = "Perguntas corretas: \(totalCorrectAnswers)"
+        WrongLB.text = "Pergutnas erradas: \(totalAnswers - totalCorrectAnswers)"
+        
+        let score = totalCorrectAnswers * 100 / totalAnswers
+        ScoreLB.text = "\(score)%"
     }
     
 
     @IBAction func closeView(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
 
